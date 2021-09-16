a04125cebc53ed284094e72581c2f598ba45883f
hung2504 <vanhung25042000@gmail.com>
2021-09-16 22:48:06 +0700

    Initial commit

---
+++ src/main/java/com/example/Springboot_Api_NguyenVanHung/SpringbootApiNguyenVanHungApplication.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Api_NguyenVanHung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiNguyenVanHungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApiNguyenVanHungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Springboot_Api_NguyenVanHung/SpringbootApiNguyenVanHungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Springboot_Api_NguyenVanHung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiNguyenVanHungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
